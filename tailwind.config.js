636950454aadccd9afa13896d70090af68d3c94b
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2025-11-07 12:18:19 +0100

    l'ajout des informations des cartes de dynamiquement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./*.js"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
-
-// *{
-//     margin: 0;
-//     padding: 0;
-// }
-// body{
-//     min-height: 100vh;
-//     width: 100vw;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-// }
-
-// .carte {
-//     width: 300px;
-//     height: 300px;
-//     background-position: center;
-//     background-repeat: no-repeat;
-//     background-size: cover;
-// }
