428fab15a68962a0a443d281cc1e44c4f51a9f0f
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2025-11-10 14:47:05 +0100

    fixarion du code et pagination

--- app.js
+++ app.js
@@ -1,63 +1,97 @@
 const main = document.querySelector("main");
-const nav = document.querySelector("nav");
+const voirPlus = document.querySelector(".voirPlus");
 const lien = "https://api.rawg.io/api/games?key=b1e47857f0c347f69a38b38626d82d65";
-let jeux = [];
+
+let toutJeux = [];
+let debut = 0;
+const jeuxParPage = 12;
+
+let pageActuelle = 1;
+
+// async function getCards(debut, fin) {
+//     try {
+//         const reponse = await fetch(lien);
+//         if (!reponse.ok) throw new Error("Erreur lors du fetch");
+//         const data = await reponse.json();
+//         toutJeux = data.results.slice(0, 64); 
+//         const jeux = toutJeux.slice(debut, fin);
+
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
 
 
-async function getCards()
-
-{
-    try{
-        const reponse = await fetch(lien);
-        if (!reponse.ok) throw new Error("Erreur lors du fetch");
-        const data = await reponse.json();
-        jeux = data.results.slice(0,64);
-        // console.log(cartes.results.slice(0,2));
-        // name, released, background_image, rating, genres, platforms 
-        jeux.forEach((jeu) => {
+function displayPage(pageActuelle){
+    main.innerHTML = "";
+    const debut = (pageActuelle - 1)*jeuxParPage;
+    const fin = debut + jeuxParPage;
+    const tranche = toutJeux.slice(debut,fin);
+    tranche.forEach((jeu)=>{
             const carte = document.createElement("div");
-            // carte.setAttribute("class", "carte");
+
             const imgDiv = document.createElement("div");
-            imgDiv.className = `h-[250px] w-10/12 bg-cover bg-no-repeat bg-center rounded-lg border-2 border-white mt-[20px]`;
+            imgDiv.className = `relative h-[250px] w-10/12 bg-cover bg-no-repeat bg-center rounded-lg border-2 border-white mt-[20px]`;
             imgDiv.style.backgroundImage = `url(${jeu.background_image})`;
+
+            const imageFavorite = document.createElement("img");
+            imageFavorite.src = "img/Star Filled.png";
+            imageFavorite.alt = "favorite";
+            imageFavorite.className = "absolute top-[10px] left-[10px] h-[35px] text-yellow-400";
+            imgDiv.appendChild(imageFavorite);
+
             carte.className = `carte w-11/12 flex flex-col items-center justify-center min-h-[600px] rounded-lg border-white border-2 bg-gradient-to-b from-[#0D1137] to-[#030B5D]`;
+
             const textDiv = document.createElement("div");
             textDiv.innerHTML = `
-            <div class="titre flex justify-between ml-[30px]">
-            <div class="flex justify-center items-center">
-            <h1 class="text-xl">Sekiro: Shadows Die Twice</h1></div>
-            <div><img src="img/Star Filled.png" alt=""></div>
-            </div>
-            <hr>
-            <div class="date flex justify-between">
-                <h4 class="text-white opacity-75">date de sortie</h4>
-                <h4>${jeu.released}</h4>
-            </div>
-            <hr>
-            <div class="date flex justify-between">
-                <h4 class="text-white opacity-75">genre</h4>
-                <h4>${jeu.genres.map(genre => genre.name)}</h4>
-            </div>
-            <hr>
-            <div class="date flex justify-between">
-                <h4 class="text-white opacity-75">platforme</h4>
-                <h4 class=" w-[40%]" >${jeu.platforms.map(p => p.platform.name)}</h4>
-            </div>
-            <hr>
-            <div class="date flex justify-between">
-                <h4 class="text-white opacity-75">note</h4>
-                <h4>${jeu.rating}</h4>
-            </div>
+                <div class="titre flex justify-center">
+                    <div class="flex justify-center items-center">
+                        <h1 class="text-xl">${jeu.name}</h1>
+                    </div>
+                </div>
+                <hr>
+                <div class="date flex justify-between">
+                    <h4 class="text-white opacity-75">date de sortie</h4>
+                    <h4>${jeu.released}</h4>
+                </div>
+                <hr>
+                <div class="date flex justify-between">
+                    <h4 class="text-white opacity-75">genre</h4>
+                    <h4>${jeu.genres.map(genre => genre.name).join(", ")}</h4>
+                </div>
+                <hr>
+                <div class="date flex justify-between">
+                    <h4 class="text-white opacity-75">platforme</h4>
+                    <h4>${jeu.platforms.map(p => p.platform.name).join(", ")}</h4>
+                </div>
+                <hr>
+                <div class="date flex justify-between">
+                    <h4 class="text-white opacity-75">note</h4>
+                    <h4>${jeu.rating}</h4>
+                </div>
             `;
             textDiv.className = `h-1/2 w-11/12 text-white flex flex-col gap-[20px] m-[30px]`;
+
             carte.appendChild(imgDiv);
             carte.appendChild(textDiv);
             main.appendChild(carte);
-        })
-    }
-    catch(error){
-        console.error(error);
-    }
+        });
 
-};
-getCards();
+}
+
+async function getAllGames() {
+    const reponse = await fetch(lien);
+    const data = await reponse.json();
+    toutJeux = data.results.slice(0, 64);
+    displayPage(pageActuelle);
+}
+
+getAllGames();
+
+
+voirPlus.addEventListener("click", () => {
+    if (pageActuelle * jeuxParPage < toutJeux.length) {
+        pageActuelle++;
+        displayPage(pageActuelle);
+    }
+});
